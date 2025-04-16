8fb4f0499aae29379ee81f0df3f3c410741a566b
Elif <elfdodanli@gmail.com>
2025-04-16 18:32:49 +0300

    update: factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
-int factorial(int n, int f);
-int main( ){
-    int n, f = 1;
+int factorial(int n);
+int main(){
+    int n = 1;
 
     cout << "enter a positive integer: ";
     cin >> n;
-    cout << "factorial of " << n << " is " << factorial( n,  f);
+    cout << "factorial of " << n << " is " << factorial(n);
     return 0;
 }
 
-int factorial(int n, int f){
-    
-
-    for(int i=1; i<=n; ++i){
+int factorial(int n) {
+    int f = 1;
+    for(int i = 1; i <= n; ++i) {
         f *= i;
-        
     }
     return f;
-    
 }
